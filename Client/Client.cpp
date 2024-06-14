d89339472699ed577089ab2b1334bb47b2b4cec1
rakkeshasa <119kms@naver.com>
2024-06-14 14:28:41 +0900

    멀티쓰레드 서버

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,44 +3,83 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUF_SIZE 30
+#define BUF_SIZE 100
+#define NAME_SIZE 20
+
+void SendMsg(SOCKET* Sock);
+void RecvMsg(SOCKET* Sock);
 void ErrorHandling(const char* message);
 
-int main()
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
+Mutex m;
+
+int main(int argc, char *argv[])
 {
 	WSADATA wsaData;
-	SOCKET hRecvSock;
-	SOCKADDR_IN adr;
-	struct ip_mreq joinAdr;
-	char buf[BUF_SIZE];
-	int strLen;
+	SOCKET hSock;
+	SOCKADDR_IN servAdr;
+	HANDLE hSndThread, hRcvThread;
 
+	if (argc != 4) {
+		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+		exit(1);
+	}
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		ErrorHandling("WSAStartup() error!");
 
-	hRecvSock = socket(PF_INET, SOCK_DGRAM, 0);
+	sprintf(name, "[%s]", argv[3]);
+	hSock = socket(PF_INET, SOCK_STREAM, 0);
 
-	memset(&adr, 0, sizeof(adr));
-	adr.sin_family = AF_INET;
-	adr.sin_addr.s_addr = htonl(INADDR_ANY);
-	adr.sin_port = htons(7777);
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	inet_pton(AF_INET, argv[1], &servAdr.sin_addr);
+	servAdr.sin_port = htons(atoi(argv[2]));
 
-	if (bind(hRecvSock, (SOCKADDR*)&adr, sizeof(adr)) == SOCKET_ERROR)
-		ErrorHandling("bind() error");
+	if (connect(hSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+		ErrorHandling("connect() error");
 
-	while (1)
-	{
-		strLen = recvfrom(hRecvSock, buf, BUF_SIZE - 1, 0, NULL, 0);
-		if (strLen < 0)
-			break;
+	thread sndThread(SendMsg, &hSock);
+	thread rcvThread(RecvMsg, &hSock);
 
-		buf[strLen] = 0;
-		fputs(buf, stdout);
-	}
+	sndThread.join();
+	rcvThread.join();
 
-	closesocket(hRecvSock);
+	closesocket(hSock);
 	WSACleanup();
 	return 0;
+}
+
+void SendMsg(SOCKET* Sock)
+{
+	SOCKET hSock = *(Sock);
+	char nameMsg[NAME_SIZE + BUF_SIZE];
+	while (1)
+	{
+		fgets(msg, BUF_SIZE, stdin);
+		if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
+		{
+			closesocket(hSock);
+			exit(0);
+		}
+		sprintf(nameMsg, "%s %s", name, msg);
+		send(hSock, nameMsg, strlen(nameMsg), 0);
+	}
+}
+
+void RecvMsg(SOCKET* Sock)
+{
+	int hSock = *(Sock);
+	char nameMsg[NAME_SIZE + BUF_SIZE];
+	int strLen;
+	while (1)
+	{
+		strLen = recv(hSock, nameMsg, NAME_SIZE + BUF_SIZE - 1, 0);
+		if (strLen == -1)
+			break;
+		nameMsg[strLen] = 0;
+		fputs(nameMsg, stdout);
+	}
 }
 
 void ErrorHandling(const char* message)
--- Socket/Socket.cpp
+++ Socket/Socket.cpp
@@ -3,42 +3,90 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <thread>
 
-#define BUF_SIZE 30
-#define TTL 64
+#define BUF_SIZE 100
+#define MAX_CLNT 256
+
+void HandleClnt(SOCKET* Sock);
+void SendMsg(char* msg, int len);
 void ErrorHandling(const char *message);
+
+int clntCnt = 0;
+SOCKET clntSocks[MAX_CLNT];
+Mutex m;
+HANDLE hMutex;
 
 int main()
 {
     WSADATA wsaData;
-    SOCKET hSendSock;
-    SOCKADDR_IN mulAdr;
-    int timeLive = TTL;
-    FILE* fp;
-    char buf[BUF_SIZE];
+    SOCKET hServSock, hClntSock;
+    SOCKADDR_IN servAdr, clntAdr;
+    int clntAdrSz;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         ErrorHandling("WSAStartup() error!");
 
-    hSendSock = socket(PF_INET, SOCK_DGRAM, 0);
-    memset(&mulAdr, 0, sizeof(mulAdr));
-    mulAdr.sin_family = AF_INET;
-    inet_pton(AF_INET, "255.255.255.255", &mulAdr.sin_addr.s_addr);
-    mulAdr.sin_port = htons(7777);
+    hMutex = CreateMutex(NULL, FALSE, NULL);
+    hServSock = socket(PF_INET, SOCK_STREAM, 0);
 
-    setsockopt(hSendSock, SOL_SOCKET, SO_BROADCAST, (const char*)&timeLive, sizeof(timeLive));
-    if ((fp = fopen("news.txt", "r")) == NULL)
-        ErrorHandling("fopen() error");
+    memset(&servAdr, 0, sizeof(servAdr));
+    servAdr.sin_family = AF_INET;
+    servAdr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAdr.sin_port = htons(7777);
 
-    while (!feof(fp))
+    if (bind(hServSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+        ErrorHandling("bind() error");
+    if (listen(hServSock, 5) == SOCKET_ERROR)
+        ErrorHandling("listen() error");
+
+    while (1)
     {
-        fgets(buf, BUF_SIZE, fp);
-        sendto(hSendSock, buf, strlen(buf), 0, (SOCKADDR*)&mulAdr, sizeof(mulAdr));
-        Sleep(2000);
+        clntAdrSz = sizeof(clntAdr);
+        hClntSock = accept(hServSock, (SOCKADDR*)&clntAdr, &clntAdrSz);
+
+        m.lock();
+        clntSocks[clntCnt++] = hClntSock;
+        m.unlock();
+
+        thread(HandleClnt, &hClntSock).detach();
+        printf("Connected client IP: %s \n", inet_ntoa(clntAdr.sin_addr));
     }
-    closesocket(hSendSock);
+    closesocket(hServSock);
     WSACleanup();
     return 0;
+}
+
+void HandleClnt(SOCKET* Sock)
+{
+    SOCKET hClntSock = *(Sock);
+    int strLen = 0, i;
+    char msg[BUF_SIZE];
+
+    while ((strLen = recv(hClntSock, msg, sizeof(msg), 0)) != 0)
+        SendMsg(msg, strLen);
+
+    m.lock();
+    for (i = 0; i < clntCnt; i++) 
+    {
+        if (hClntSock == clntSocks[i])
+        {
+            while (i++ < clntCnt - 1)
+                clntSocks[i] = clntSocks[i + 1];
+            break;
+        }
+    }
+    clntCnt--;
+    m.unlock();
+    closesocket(hClntSock);
+}
+void SendMsg(char* msg, int len) 
+{
+    int i;
+    m.lock();
+    for (i = 0; i < clntCnt; i++)
+        send(clntSocks[i], msg, len, 0);
+    m.unlock();
 }
 
 void ErrorHandling(const char *message)
