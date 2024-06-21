9556e3983ca288e7a3bb4396f7f1962fd15247d7
rakkeshasa <119kms@naver.com>
2024-06-21 13:16:13 +0900

    Overlapped Model 에코서버

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+# define BUF_SIZE 1024
 void ErrorHandling(const char* message);
 
 int main()
@@ -10,18 +11,15 @@ int main()
 	WSADATA wsaData;
 	SOCKET hSocket;
 	SOCKADDR_IN sendAdr;
-
-	WSABUF dataBuf;
-	char msg[] = "작중에서는 사고가 굳어진 어른들이 모자라고만 생각했다고 묘사된다. 그러나 <어린 왕자>가 베스트셀러로 유명해지면서 '코끼리를 잡아먹은 보아뱀' 이야기가 널리 퍼져 아이든 어른이든 그 유명한 그림을 보여줬을 때 모두 '코끼리를 잡아먹은 보아뱀'이라고 답하게 되었다고 한다. 이에 대해 작가인 생텍쥐페리는 오히려 이 사실에 절망감을 느꼈다고 한다. 생텍쥐페리는 이 이야기를 통해 어린이들의 동심에서 비롯된 '틀에 얽매이지 않는 생각'을 말하고자 했지만 정작 이 우화가 '또 다른 생각의 틀'을 만들었기 때문이었다. 클리셰 파괴를 위한 클리셰 작중 삽화가 대부분의 판본에서 동일한데 그 이유는 작가가 직접 그렸기 때문이다.작중 화자 스스로가 자신은 보아뱀 그림 이후에 그림 공부를 한 적이 없다고 말했고, 어린 왕자가 화자가 그린 그림을 보고 이상하게 그렸다고 지적하는 부분도 있을 정도로 아주 잘 그렸다고는 말하기 힘들지만 그럼에도 묘한 매력이 있다.근래에는 다른 작가가 그린 화려한 일러스트로 바꿔서 출판되는 경우도 많지만, 역시 원작의 그림을 선호하는 사람들이 많다.오히려 작가가 책을 집필할 때 같이 그렸기 때문에 그림도 작품의 일부라고 할 수 있으며";
-	DWORD sendBytes = 0;
-
-	WSAEVENT evObj;
-	WSAOVERLAPPED overlapped;
+	char message[BUF_SIZE];
+	int strLen, readLen;
 
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		ErrorHandling("WSAStartup() error!");
 
 	hSocket = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	if (hSocket == INVALID_SOCKET)
+		ErrorHandling("socket() error");
 
 	memset(&sendAdr, 0, sizeof(sendAdr));
 	sendAdr.sin_family = AF_INET;
@@ -33,28 +31,25 @@ int main()
 	else
 		puts("Connected...........");
 
-	evObj = WSACreateEvent();
-	memset(&overlapped, 0, sizeof(overlapped));
-	overlapped.hEvent = evObj;
-	dataBuf.len = strlen(msg) + 1;
-	dataBuf.buf = msg;
-
-	if (WSASend(hSocket, &dataBuf, 1, &sendBytes, 0, &overlapped, NULL) == SOCKET_ERROR)
+	while (1)
 	{
-		if (WSAGetLastError() == WSA_IO_PENDING)
-		{
-			puts("데이터 송신 중...");
-			WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
-			WSAGetOverlappedResult(hSocket, &overlapped, &sendBytes, FALSE, NULL);
-		}
-		else
-		{
-			ErrorHandling("WSASend() error");
-		}
-	}
+		fputs("Input message(Q to quit): ", stdout);
+		fgets(message, BUF_SIZE, stdin);
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
 
-	printf("보낸 데이터의 크기: %d \n", sendBytes);
-	WSACloseEvent(evObj);
+		strLen = strlen(message);
+		send(hSocket, message, strLen, 0);
+		readLen = 0;
+		while (1)
+		{
+			readLen += recv(hSocket, &message[readLen], BUF_SIZE - 1, 0);
+			if (readLen >= strLen)
+				break;
+		}
+		message[strLen] = 0;
+		printf("Message from server: %s", message);
+	}
 	
 	closesocket(hSocket);
 	WSACleanup();
--- Socket/Socket.cpp
+++ Socket/Socket.cpp
@@ -6,26 +6,36 @@
 #include <thread>
 
 #define BUF_SIZE 1024
+void CALLBACK ReadCallback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
+void CALLBACK WriteCallback(DWORD, DWORD, LPWSAOVERLAPPED, DWORD);
 void ErrorHandling(const char *message);
+
+typedef struct
+{
+    SOCKET hClntSock;
+    char buf[BUF_SIZE];
+    WSABUF wsaBuf;
+} PER_IO_DATA, *LPPER_IO_DATA;
 
 int main()
 {
     WSADATA wsaData;
     SOCKET hLisnSock, hRecvSock;
     SOCKADDR_IN lisnAdr, recvAdr;
+    LPWSAOVERLAPPED lpOvLp;
+    DWORD recvBytes, mode = 1, flagInfo = 0;
+    LPPER_IO_DATA hbInfo;
     int recvAdrSize;
-
-    WSABUF dataBuf;
+ 
     WSAEVENT evObj;
     WSAOVERLAPPED overlapped;
 
-    char buf[BUF_SIZE];
-    DWORD recvBytes = 0, flags = 0;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         ErrorHandling("WSAStartup() error!");
 
-    hLisnSock = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+    hLisnSock = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED); // blocking socket
+    ioctlsocket(hLisnSock, FIONBIO, &mode); // non-blocking socket
 
     memset(&lisnAdr, 0, sizeof(lisnAdr));
     lisnAdr.sin_family = AF_INET;
@@ -38,35 +48,73 @@ int main()
         ErrorHandling("listen() error");
 
     recvAdrSize = sizeof(recvAdr);
-    hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAdr, &recvAdrSize);
-
-    evObj = WSACreateEvent();
-    memset(&overlapped, 0, sizeof(overlapped));
-    overlapped.hEvent = evObj;
-    dataBuf.len = BUF_SIZE;
-    dataBuf.buf = buf;
-
-    if (WSARecv(hRecvSock, &dataBuf, 1, &recvBytes, &flags, &overlapped, NULL) == SOCKET_ERROR)
+    while (1)
     {
-        if (WSAGetLastError() == WSA_IO_PENDING)
+        SleepEx(100, TRUE);
+        hRecvSock = accept(hLisnSock, (SOCKADDR*)&recvAdr, &recvAdrSize); // repeat accpet()
+        if (hRecvSock == INVALID_SOCKET)
         {
-            puts("WSA_IO_PENDING!!");
-            puts("데이터 수신 중... 기다려주세요");
-            WSAWaitForMultipleEvents(1, &evObj, TRUE, WSA_INFINITE, FALSE);
-            WSAGetOverlappedResult(hRecvSock, &overlapped, &recvBytes, FALSE, NULL);
+            if (WSAGetLastError() == WSAEWOULDBLOCK) // no connect() request
+                continue;
+            else
+                ErrorHandling("accpet() error");
         }
-        else
-        {
-            ErrorHandling("WSARecv() error");
-        }
+
+        // Client connect() request
+        puts("Client connected...");
+
+        // Each Client have WSAOVERLAPPED struct
+        lpOvLp = (LPWSAOVERLAPPED)malloc(sizeof(WSAOVERLAPPED));
+        memset(lpOvLp, 0, sizeof(WSAOVERLAPPED));
+
+        // hRecvSock info save to PER_IO_DATA struct
+        hbInfo = (LPPER_IO_DATA)malloc(sizeof(PER_IO_DATA));
+        hbInfo->hClntSock = (DWORD)hRecvSock;
+        (hbInfo->wsaBuf).buf = hbInfo->buf;
+        (hbInfo->wsaBuf).len = BUF_SIZE;
+
+        lpOvLp->hEvent = (HANDLE)hbInfo;
+        WSARecv(hRecvSock, &(hbInfo->wsaBuf), 1, &recvBytes, &flagInfo, lpOvLp/*callback 3rd parameter*/, ReadCallback);
+        // Thanks to lpOvLp, callback function can access socket handle and buffer.
     }
 
-    printf("받은 메시지: %s \n", buf);
-    WSACloseEvent(evObj);
     closesocket(hRecvSock);
     closesocket(hLisnSock);
     WSACleanup();
     return 0;
+}
+
+// Input Data is completed -> Echo data to client
+void CALLBACK ReadCallback(DWORD dwError, DWORD RecvBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags)
+{
+    // 입력이 완료된 소켓의 핸들과 버퍼 정보 추출
+    LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
+    SOCKET hSock = hbInfo->hClntSock;
+    LPWSABUF bufInfo = &(hbInfo->wsaBuf);
+    DWORD sendBytes;
+
+    if (RecvBytes == 0)
+    {
+        closesocket(hSock);
+        free(lpOverlapped->hEvent);
+        free(lpOverlapped);
+        puts("Client disconnected...");
+    }
+    else
+    {
+        bufInfo->len = RecvBytes;
+        WSASend(hSock, bufInfo, 1, &sendBytes, 0, lpOverlapped, WriteCallback);
+    }
+}
+
+void CALLBACK WriteCallback(DWORD dwError, DWORD SendBytes, LPWSAOVERLAPPED lpOverlapped, DWORD flags)
+{
+    LPPER_IO_DATA hbInfo = (LPPER_IO_DATA)(lpOverlapped->hEvent);
+    SOCKET hSock = hbInfo->hClntSock;
+    LPWSABUF bufInfo = &(hbInfo->wsaBuf);
+    DWORD recvBytes, flagInfo = 0;
+
+    WSARecv(hSock, bufInfo, 1, &recvBytes, &flagInfo, lpOverlapped, ReadCallback);
 }
 
 void ErrorHandling(const char *message)
